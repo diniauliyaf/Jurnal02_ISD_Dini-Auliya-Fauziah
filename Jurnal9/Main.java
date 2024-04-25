b2b642397f5c72b739fa12279295bf941e9fcab0
diniauliyaf <145308318+diniauliyaf@users.noreply.github.com>
2024-04-25 10:26:26 +0700

    Add files via upload

---
+++ Jurnal9/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        AdjacencyList myList = new AdjacencyList(9);
+        myList.addEdge('A', 'D');
+        myList.addEdge('A', 'B');
+        myList.addEdge('A', 'E');
+        myList.addEdge('B', 'E');
+        myList.addEdge('C', 'B');
+        myList.addEdge('D', 'G');
+        myList.addEdge('E', 'F');
+        myList.addEdge('E', 'H');
+        myList.addEdge('F', 'C');
+        myList.addEdge('F', 'H');
+        myList.addEdge('G', 'H');
+        myList.addEdge('H', 'I');
+        myList.addEdge('I', 'F');
+        myList.printGraph();
+    }
+}
